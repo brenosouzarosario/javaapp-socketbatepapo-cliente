34064596cb0552d982831681d5b257bc4fe0c99e
Breno Souza <brenosouza.rosario@gmail.com>
2020-07-11 18:44:55 -0300

    first commit

---
+++ src/Controller/ClienteController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Bean.ChatMessage;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Breno
+ */
+public class ClienteController {
+    private Socket socket;
+    private ObjectOutputStream output;
+    
+    public Socket connect() {
+        try {
+            this.socket = new Socket("localhost", 5555);
+            this.output = new ObjectOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(ClienteController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return socket;
+    }
+    
+    public void send(ChatMessage message) {
+        try {
+            output.writeObject(message);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
